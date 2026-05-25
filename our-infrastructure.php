f280483be4ac43ef33a586488c2d0a5c40688afe
Hxtreme <rwpttech@gmail.com>
2026-05-25 15:22:31 +0530

    unwanted-comments-removed

--- our-infrastructure.php
+++ our-infrastructure.php
@@ -1,12 +1,12 @@
 <?php include 'include/header.php'; ?>
 <style>
-        /* html{
+  /* html{
             background: #dee8f0;
     }
     .soon {
         margin-top: 10%;
     } */
-/* .soon img {
+  /* .soon img {
     width: 100%;
     display: flex;
     margin: 0 auto;
@@ -15,32 +15,30 @@
     align-items: center;
     object-fit: contain;
 } */
-    .soon{
-            height: 60vh;
-    display: flex
-;
+  .soon {
+    height: 60vh;
+    display: flex;
     justify-content: center;
     align-items: center;
     text-align: center;
-}
-.soon img {
-   
+  }
+
+  .soon img {
     width: 100%;
     text-align: center;
     display: flex;
     justify-content: center;
     align-items: center;
-    
-}
+  }
 </style>
 <div class="soon">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 m-auto">
-                  <img  src="images/urja/urja-infra.jpg" alt="coming soon">
-                </div>
-            </div>
-           
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 m-auto">
+        <img src="images/urja/urja-infra.jpg" alt="coming soon">
+      </div>
     </div>
+
+  </div>
+</div>
 <?php include 'include/footer.php'; ?>
